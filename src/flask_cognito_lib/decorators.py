21d91a7815263eed6bab3ded5b40367a4ea2f903
ron <ron.glasmann@pricestats.com>
2024-09-19 19:40:21 -0400

    fix for "auth disabled" bug

--- src/flask_cognito_lib/decorators.py
+++ src/flask_cognito_lib/decorators.py
@@ -137,6 +137,25 @@ def cognito_login(fn):
 
     @wraps(fn)
     def wrapper(*args, **kwargs):
+
+        if cognito_auth.cfg.disabled:
+            print(f"cognito auth disabled adding the 'auth disabled' user to session")
+            user_id = os.getenv("AWS_COGNITO_DISABLED_USER_ID", None)
+            email = os.getenv("AWS_COGNITO_DISABLED_USER_EMAIL", None)
+            name = os.getenv("AWS_COGNITO_DISABLED_USER_NAME", None)
+            if user_id is None or email is None or name is None:
+                raise Exception(f"'AWS_COGNITO_DISABLED_USER_ID', 'AWS_COGNITO_DISABLED_USER_EMAIL', and "
+                                f"'AWS_COGNITO_DISABLED_USER_NAME' must be set when 'AWS_COGNITO_DISABLED'")
+            user_info = {
+                'id': user_id,
+                'email': email,
+                'name': name,
+                'is_active': 'True',
+            }
+            session.update({"user": user_info})
+            resp = fn(*args, **kwargs)
+            return resp
+
         # store parameters in the session that are passed to Cognito
         # and required for JWT verification
         code_verifier = generate_code_verifier()
@@ -298,24 +317,6 @@ def auth_required(groups: Optional[Iterable[str]] = None, any_group: bool = Fals
         @wraps(fn)
         def decorator(*args, **kwargs):
             validate_access(cognito_auth, request, groups=groups, any_group=any_group)
-
-            if cognito_auth.cfg.disabled and 'user' not in session:
-                print(f"cognito auth disabled and user is not is session, adding user to session")
-                user_id = os.getenv("AWS_COGNITO_DISABLED_USER_ID", None)
-                email = os.getenv("AWS_COGNITO_DISABLED_USER_EMAIL", None)
-                name = os.getenv("AWS_COGNITO_DISABLED_USER_NAME", None)
-                if user_id is None or email is None or name is None:
-                    raise Exception(f"'AWS_COGNITO_DISABLED_USER_ID', 'AWS_COGNITO_DISABLED_USER_EMAIL', and "
-                                    f"'AWS_COGNITO_DISABLED_USER_NAME' must be set when 'AWS_COGNITO_DISABLED'")
-                user_info = {
-                    'id': user_id,
-                    'email': email,
-                    'name': name,
-                    'is_active': 'True',
-                    # 'groups': '',
-                    # 'hashed_password': '',
-                }
-                session.update({"user": user_info})
 
             return fn(*args, **kwargs)
             # # return early if the extension is disabled
